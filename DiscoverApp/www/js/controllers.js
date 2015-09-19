19a836befbee6e5f66b80b58a963db3440c6f4b2
Arjun Kalburgi <askalburgi@gmail.com>
2015-09-19 14:00:26 -0600

    implementation of firebase.

--- DiscoverApp/www/js/controllers.js
+++ DiscoverApp/www/js/controllers.js
@@ -2,11 +2,12 @@ angular.module('starter.controllers', ['ngOpenFB'])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout, ngFB) {
 	$scope.fbLogin = function () {
-		ngFB.login({scope: 'user_likes'}).then(//email,,read_stream,publish_actions
+		ngFB.login({scope: 'email,user_likes'}).then(//,read_stream,publish_actions
 			function (response) {
 				if (response.status === 'connected') {
 					console.log('Facebook login succeeded');
-					$scope.closeLogin();
+					// console.log(ngFB.login.email); 
+					//$scope.closeLogin();
 				} else {
 					alert('Facebook login failed');
 				}
@@ -15,8 +16,14 @@ angular.module('starter.controllers', ['ngOpenFB'])
 })
 
 
-.controller('DashCtrl', function($scope) {})
+// This is search
+.controller('DashCtrl', function($scope, UserLikes) {
 
+	$scope.likes = UserLikes;
+
+})
+
+// This is
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
